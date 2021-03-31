34275c1fde6940c0b3db87864b5985c44e645c4d
davidchanho <33166931+davidchanho@users.noreply.github.com>
2021-03-31 03:22:42 -0400

    remove react devtools

--- components/_layout/head/index.tsx
+++ components/_layout/head/index.tsx
@@ -5,8 +5,13 @@ function Head() {
   return (
     <NHead>
       <title>Shortly</title>
-      <meta name='description' content='shorten urls with analysis.' />
+      <meta name="description" content="shorten urls with analysis." />
       <link rel="icon" href="/favicon.png" />
+      <script async>
+        {process.env.NODE_ENV == "production"
+          ? "window.__REACT_DEVTOOLS_GLOBAL_HOOK__.inject = function(){}"
+          : ""}
+      </script>
     </NHead>
   );
 }
